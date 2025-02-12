4be9835e54d6e84f8580e3ef595fa259b6fef93f
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-02-12 23:06:42 +0200

    Created 'Majority Element II' by A2SV Hub

---
+++ 12-Feb-2025/Majority Element II 261547.py
@@ -0,0 +1,29 @@
+# Problem: Majority Element II - https://leetcode.com/problems/majority-element-ii/?envType=daily-question&envId=2023-10-05
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        count1, count2=0, 0
+        candidate1, candidate2 = None, None
+        for n in nums:
+            if candidate1 == n:
+                count1 += 1
+            elif candidate2 == n:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1 -=1 
+                count2 -=1
+
+        result =[]
+                
+        for candidate in [candidate1, candidate2]:
+            if nums.count(candidate) > len(nums) // 3:
+                result.append(candidate)
+        return result
+
+    
