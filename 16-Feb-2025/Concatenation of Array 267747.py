629d9d7427cc3f1fc293dea01de1384d2ee44519
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-02-16 18:56:37 +0200

    Created 'Concatenation of Array' by A2SV Hub

---
+++ 16-Feb-2025/Concatenation of Array 267747.py
@@ -0,0 +1,8 @@
+# Problem: Concatenation of Array - https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        nums.extend(nums)
+        return nums
+
+        
