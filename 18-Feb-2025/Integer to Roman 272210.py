ef386e3f9c2768177cb5fcc019f54b67f7a34c76
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-02-18 17:55:00 +0200

    Created 'Integer to Roman' by A2SV Hub

---
+++ 18-Feb-2025/Integer to Roman 272210.py
@@ -0,0 +1,13 @@
+# Problem: Integer to Roman - https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        numList = [['I',1],['IV', 4],['V',5], ['IX', 9],['X',10],['XL',40], ['L',50],['XC', 90],['C', 100],['CD',400],['D', 500], ['CM', 900], ['M',1000]]
+        res=''
+        for sym, val in reversed(numList):
+            if num // val:
+                count = num // val
+                res += (sym * count)
+                num = num % val
+        return res
+         
