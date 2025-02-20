76bded4fb195a528ef58fb00415557fa50fc56bc
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-02-20 09:06:44 +0200

    Created 'Find the Winner of the Circular Game' by A2SV Hub

---
+++ 20-Feb-2025/Find the Winner of the Circular Game 275692.py
@@ -0,0 +1,14 @@
+# Problem: Find the Winner of the Circular Game - https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        friends = list(range(1, n+1))
+        current_position = 0
+
+        while n > 1:
+            current_position = (current_position + (k-1)) % n
+            friends.pop(current_position)
+
+            n -= 1
+
+        return friends [0]
