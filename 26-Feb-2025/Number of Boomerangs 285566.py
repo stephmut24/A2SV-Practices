94239d67f9e56201fa8e2bc2fae1a70346f05cb9
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-02-26 22:35:01 +0200

    Created 'Number of Boomerangs' by A2SV Hub

---
+++ 26-Feb-2025/Number of Boomerangs 285566.py
@@ -0,0 +1,14 @@
+# Problem: Number of Boomerangs - https://leetcode.com/problems/number-of-boomerangs/description/
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        res = 0
+        for p in points:
+            cmap={}
+            for q in points:
+                f = p[0]-q[0]
+                s = p[1]-q[1]
+                cmap[f*f + s*s] = 1 + cmap.get(f*f + s*s, 0)
+            for k in cmap:
+                res += cmap[k] * (cmap[k]-1)
+        return res
