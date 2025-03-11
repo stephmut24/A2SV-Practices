4ac124837ea6409c4e68a097faea85551535a41e
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-03-11 18:16:49 +0200

    Created 'Valid Anagram' by A2SV Hub

---
+++ 11-Mar-2025/Valid Anagram 302926.py
@@ -0,0 +1,6 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+       return Counter(s) == Counter(t)
+        
