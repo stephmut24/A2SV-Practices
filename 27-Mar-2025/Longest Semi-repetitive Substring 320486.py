bf4832fc10fcac3e5ede9430343136bca3dea0e6
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-03-27 22:08:23 +0200

    Created 'Longest Semi-repetitive Substring' by A2SV Hub

---
+++ 27-Mar-2025/Longest Semi-repetitive Substring 320486.py
@@ -0,0 +1,21 @@
+# Problem: Longest Semi-repetitive Substring - https://leetcode.com/problems/find-the-longest-semi-repetitive-substring/
+
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+        
+        ans = 1
+        pair = left = 0
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                pair += 1
+            
+            while pair > 1 and left < i:
+                if s[left] == s[left + 1]:
+                    pair -= 1
+                left += 1
+        
+            ans = max(ans, i - left + 1)
+
+        return ans
+
+        
