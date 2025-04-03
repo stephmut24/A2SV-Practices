6c388537afc88a257fb2fb46b9c12eebd67dd8ae
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-04-03 09:56:42 +0200

    Created 'Product of Array Except Self' by A2SV Hub

---
+++ 03-Apr-2025/Product of Array Except Self 324529.py
@@ -0,0 +1,17 @@
+# Problem: Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        output = [1] * len(nums)
+
+        left = 1
+        for i in range(len(nums)):
+            output[i] *= left
+            left *= nums[i]
+
+        right = 1
+        for i in range(len(nums)-1, -1, -1):
+            output[i] *= right
+            right *= nums[i]
+        return output
