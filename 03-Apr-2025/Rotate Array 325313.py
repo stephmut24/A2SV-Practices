38c5e6b8a987d71349ff0ff3f2bc8b5b80ed9433
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-04-03 19:05:18 +0200

    Created 'Rotate Array' by A2SV Hub

---
+++ 03-Apr-2025/Rotate Array 325313.py
@@ -0,0 +1,12 @@
+# Problem: Rotate Array - https://leetcode.com/problems/rotate-array/
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n =len(nums)
+        k = k % n
+        rotated = [0] * n
+
+        for i in range(n):
+            rotated[(i + k) % n] = nums[i]
+        for i in range(n):
+            nums[i] = rotated[i]
