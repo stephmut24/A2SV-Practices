b53cf3ee2cc9f2a1d176f6dc4cfe34abcf4ae08a
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-04-29 21:39:14 +0300

    Created 'Power of Four' by A2SV Hub

---
+++ 29-Apr-2025/Power of Four 344936.py
@@ -0,0 +1,12 @@
+# Problem: Power of Four - https://leetcode.com/problems/power-of-four/
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0 : return False
+        if n == 1: return True
+
+        while n > 1 :
+            if n % 4 !=0:
+                return False
+            n //= 4
+        return True
