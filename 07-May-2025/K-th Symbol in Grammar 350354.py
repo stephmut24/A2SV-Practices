eeda2461e2bcd9cfb5869989722b3d3b9d03d6f4
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-05-07 20:07:44 +0300

    Created 'K-th Symbol in Grammar' by A2SV Hub

---
+++ 07-May-2025/K-th Symbol in Grammar 350354.py
@@ -0,0 +1,16 @@
+# Problem: K-th Symbol in Grammar - https://leetcode.com/problems/k-th-symbol-in-grammar/description/
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        cur = 0
+        left, right = 1, 2**(n-1)
+
+        for _ in range(n - 1):
+            mid = (left + right) // 2
+            if k <= mid:
+                right = mid
+            else:
+                left = mid +1
+                cur = 0 if cur else 1
+        return cur
+        
