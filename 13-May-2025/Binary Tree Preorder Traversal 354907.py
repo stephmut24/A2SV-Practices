81b91409d0e3b7a81f5477f349c2bf5f942b0169
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-05-13 22:14:40 +0200

    Created 'Binary Tree Preorder Traversal' by A2SV Hub

---
+++ 13-May-2025/Binary Tree Preorder Traversal 354907.py
@@ -0,0 +1,20 @@
+# Problem: Binary Tree Preorder Traversal - https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        res = [root.val]
+        res = res + self.preorderTraversal(root.left)
+        res = res + self.preorderTraversal(root.right)
+
+        return res
+
+        
