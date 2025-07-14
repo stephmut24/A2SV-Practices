05af80b0e9fe330320388e47e05dc8e4ef8b1f14
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-07-14 14:09:49 +0200

    Created 'A - Swap to Max' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/A - Swap to Max 376395.py
@@ -0,0 +1,36 @@
+# Problem: A - Swap to Max - https://codeforces.com/gym/622136/problem/A
+
+def min_maximum_after_swap(t, text_cases):
+    res = []
+    
+    for case in text_cases:
+        n, a,b = case
+        
+        for i in range(n):
+            if a[i] >b[i]:
+                a[i], b[i] = b[i],a[i]
+                
+        max_a = max(a)
+        max_b = max(b)
+        
+        res.append(max_a * max_b)
+        
+    return res
+
+
+t = int(input())
+test_cases = []
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    test_cases.append((n, a, b))
+
+
+res = min_maximum_after_swap(t, test_cases)
+
+for result in res:
+    print(result)
+    
+    
