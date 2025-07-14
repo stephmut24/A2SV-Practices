89a7cd2b21a277a2613f29cd0f95ec7b38be2fa8
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-07-14 14:25:21 +0200

    Created 'B - Lifeboat Lineup' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/B - Lifeboat Lineup 376397.py
@@ -0,0 +1,39 @@
+# Problem: B - Lifeboat Lineup - https://codeforces.com/gym/622136/problem/B
+
+
+
+def lifeboat_lineup(n, crew_lst):
+    
+    rats =[]
+    women_children =[]
+    men = []
+    cap=""
+    
+    
+    for name, status in crew_lst:
+        if status == "rat":
+            rats.append(name)
+        elif status == "woman" or status =="child":
+            women_children.append(name)
+            
+        elif status =='man':
+            men.append(name)
+            
+        elif status == "captain":
+            cap = name
+            
+    evacuation_order = rats + women_children +men + [cap]
+    
+    return evacuation_order
+
+
+n =int(input())
+crew_lst = [tuple(input().split()) for _ in range(n)]
+
+order = lifeboat_lineup(n, crew_lst)
+
+for name in order:
+    print(name)
+   
+
+
