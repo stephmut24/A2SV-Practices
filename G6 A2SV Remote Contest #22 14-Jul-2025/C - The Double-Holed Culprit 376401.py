783d90ae70cfa5e6924cf5c9eab3a38924b3171b
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-07-14 14:39:10 +0200

    Created 'C - The Double-Holed Culprit' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/C - The Double-Holed Culprit 376401.py
@@ -0,0 +1,28 @@
+# Problem: C - The Double-Holed Culprit - https://codeforces.com/gym/622136/problem/C
+
+def find_culprits(n, p):
+    result = []
+
+    for start in range(1, n + 1):
+        visited = set()
+        current = start
+
+        while current not in visited:
+            visited.add(current)
+            current = p[current - 1]  
+
+        result.append(current)
+
+    return result
+
+
+
+n = int(input())
+p = list(map(int, input().split()))
+
+
+
+culprits = find_culprits(n, p)
+
+
+print(' '.join(map(str, culprits)))
