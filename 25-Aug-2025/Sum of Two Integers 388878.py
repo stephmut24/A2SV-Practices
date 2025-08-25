ebfc01c34de580e9006cad617086005d620a5862
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-08-25 21:14:46 +0200

    Created 'Sum of Two Integers' by A2SV Hub

---
+++ 25-Aug-2025/Sum of Two Integers 388878.py
@@ -0,0 +1,10 @@
+# Problem: Sum of Two Integers - https://leetcode.com/problems/sum-of-two-integers/description/
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xffffffff
+        while (mask&b) >0:
+            a,b = a^b, (a&b)<<1
+
+        return (mask&a) if b > 0 else a 
+        
