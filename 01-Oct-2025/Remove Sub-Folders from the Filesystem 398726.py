04a109e07050e73aabfbfc81902949321715e812
Mugisho Mutarushwa Stephan <126453005+stephmut24@users.noreply.github.com>
2025-10-01 20:36:09 +0200

    Created 'Remove Sub-Folders from the Filesystem' by A2SV Hub

---
+++ 01-Oct-2025/Remove Sub-Folders from the Filesystem 398726.py
@@ -0,0 +1,17 @@
+# Problem: Remove Sub-Folders from the Filesystem - https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/
+
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        res = []
+        for f in folder:
+            if not res:
+                res.append(f)
+            else:
+                prev = res[-1]
+                if f.startswith(prev) and len(f) > len(prev) and f[len(prev)] == '/':
+                    continue
+                else:
+                    res.append(f)
+        return res
